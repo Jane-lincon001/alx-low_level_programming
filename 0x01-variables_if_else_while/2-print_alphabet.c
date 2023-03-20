d50dc94271fe8986a13d7d49356495739d3d2940
“Jane <jamwangi001@gmail.com>
2023-03-20 09:11:43 -0700

    Insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - main function
+ * main - Prints the alphabet
  *
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
-	for (i = 'a' ; i <= 'z' ; i++)
-	putchar(i);
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alph[i]);
+	}
 	putchar('\n');
 	return (0);
+}
 
 
