6f57dbfce5a397226c44eba128f0bd62aa3d0b3b
“Jane <jamwangi001@gmail.com>
2023-03-20 09:43:44 -0700

    Soup alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- *main - main function
+ * main - Prints the alphabet without q and e
  *
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
-		if (i != 'q' && i != 'e')
+	for (i = 97; (i = 123); i++)
+	{
+		if (i != 101 && i != 113)
+		{
 			putchar(i);
+		}
+	}
 	putchar('\n');
 	return (0);
-}	
+}
